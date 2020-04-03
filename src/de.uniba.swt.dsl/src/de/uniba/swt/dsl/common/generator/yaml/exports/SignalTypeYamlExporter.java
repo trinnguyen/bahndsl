9535c044f1486960158fc5482a34e520288f9a73
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-04-03 16:19:54 +0200

    generate signal type aspect ids only

--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/generator/yaml/exports/SignalTypeYamlExporter.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/generator/yaml/exports/SignalTypeYamlExporter.java
@@ -1,9 +1,12 @@
 package de.uniba.swt.dsl.common.generator.yaml.exports;
 
+import de.uniba.swt.dsl.bahn.AspectElement;
 import de.uniba.swt.dsl.bahn.SignalType;
 import de.uniba.swt.dsl.common.util.Tuple;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class SignalTypeYamlExporter extends AbstractElementYamlExporter<SignalType> {
 
@@ -14,7 +17,7 @@ class SignalTypeYamlExporter extends AbstractElementYamlExporter<SignalType> {
 
     @Override
     protected List<Tuple<String, Object>> getProps(SignalType element) {
-        return List.of(Tuple.of("aspects", element.getItems()),
-                Tuple.of("initial", element.getInitial().getName()));
+        List<String> aspectIds = element.getItems() != null ? element.getItems().stream().map(AspectElement::getName).collect(Collectors.toList()) : new ArrayList<>();
+        return List.of(Tuple.of("aspects", aspectIds));
     }
 }
