710e7c173c7248394430358766424c6ed9306842
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-04-27 16:43:16 +0200

    load resource for eclipse-based IDE

--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/StandardLibLazyLinker.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/StandardLibLazyLinker.java
@@ -6,12 +6,13 @@ import org.eclipse.xtext.diagnostics.IDiagnosticConsumer;
 import org.eclipse.xtext.linking.lazy.LazyLinker;
 
 public class StandardLibLazyLinker extends LazyLinker {
+	
     @Override
     protected void beforeModelLinked(EObject model, IDiagnosticConsumer diagnosticsConsumer) {
         super.beforeModelLinked(model, diagnosticsConsumer);
         var set = model.eResource().getResourceSet();
         if (set.getResources().size() < 2 && !model.eResource().getURI().toString().endsWith(StandardLibHelper.FILE_NAME)) {
-            StandardLibHelper.loadStandardLibResource(set);
+        	StandardLibHelper.loadStandardLibResource(set);
         }
     }
 }
--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/generator/StandardLibHelper.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/generator/StandardLibHelper.java
@@ -6,33 +6,68 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class StandardLibHelper {
     private static Logger logger = Logger.getLogger(StandardLibHelper.class);
 
+    private static final String RESOURCES_FOLDER_NAME = "resources";
+    
     public static final String FILE_NAME = "standardlib.bahn";
 
-    public static boolean loadStandardLibResource(ResourceSet resourceSet) {
+	public static void loadStandardLibResource(ResourceSet resourceSet) {
         logger.debug("Start loading standard resource");
+        
         URI uri = URI.createURI(FILE_NAME);
         var resource = resourceSet.createResource(uri);
 
-        var stream = StandardLibHelper.class.getClassLoader().getResourceAsStream(FILE_NAME);
+        InputStream stream = null;
         try {
+        	// load stream
+            stream = loadEmbeddedStream();
+            if (stream == null ) {
+            	stream = loadPluginResourceStream();
+            }
+            
             resource.load(stream, resourceSet.getLoadOptions());
             if (resource.getErrors().size() > 0) {
                 logger.error("Error on parsing built-in standard library");
                 for (Resource.Diagnostic error : resource.getErrors()) {
                     logger.error(error);
                 }
-                return false;
+            } else {
+            	logger.info("Success loading standard library");
             }
         } catch (IOException e) {
             logger.error("Error on loading built-in standing library: " + e.getMessage());
-            return false;
+        } finally {
+        	if (stream != null) {
+        		try {
+					stream.close();
+				} catch (IOException e) {
+					logger.error("Failed to close stream: " + e.getMessage());
+				}
+        	}
         }
+    }
+    
+    /**
+     * Load stream from embedded resource of Java application
+     * Used in cli compiler and ide server for visual studio code extension
+     * @return
+     */
+    private static InputStream loadEmbeddedStream() {
+    	return StandardLibHelper.class.getClassLoader().getResourceAsStream(FILE_NAME);
+	}
 
-        logger.debug("Success loading standard library");
-        return true;
+    /**
+     * Load stream from plugin resource
+     * @return
+     * @throws IOException
+     */
+	private static InputStream loadPluginResourceStream() throws IOException  {
+    	var url = new URL("platform:/plugin/de.uniba.swt.dsl/" + RESOURCES_FOLDER_NAME + "/" + FILE_NAME);
+	    return url.openConnection().getInputStream();
     }
 }
