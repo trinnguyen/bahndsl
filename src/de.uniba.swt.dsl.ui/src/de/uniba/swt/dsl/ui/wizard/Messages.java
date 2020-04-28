d096eaa1410623d64c4e0c065f678dc00edb03c6
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-04-28 12:32:05 +0200

    upgrade to Xtext 2.21

--- src/de.uniba.swt.dsl.ui/src/de/uniba/swt/dsl/ui/wizard/Messages.java
+++ src/de.uniba.swt.dsl.ui/src/de/uniba/swt/dsl/ui/wizard/Messages.java
@@ -3,19 +3,18 @@ package de.uniba.swt.dsl.ui.wizard;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	
-	private static final String BUNDLE_NAME = "de.uniba.swt.dsl.ui.wizard.messages";
+	private static final String BUNDLE_NAME = "de.uniba.swt.dsl.ui.wizard.messages"; //$NON-NLS-1$
 	
 	public static String SampleBahnProject_Label;
-	
 	public static String SampleBahnProject_Description;
-	
 	public static String BahnModelFile_Label;
-	
 	public static String BahnModelFile_Description;
 	
 	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	// initialize resource bundle
+	NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
 	}
 }
