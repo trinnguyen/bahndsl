a85e24fe29b36de47f667d54cf6c861db1a22f91
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-04-30 17:06:10 +0200

    update test script

--- src/build-test.sh
+++ src/build-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 cd "$(dirname "$0")"
 ./gradlew de.uniba.swt.dsl.test:test de.uniba.swt.dsl.test:jacocoTestReport
 
 # build folder
-mkdir build
+mkdir -p build
 cd build
 
 # copy the file
