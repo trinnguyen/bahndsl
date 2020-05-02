b696362158a55a85bd5368fe052d99db46fd5c5c
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-05-02 11:03:11 +0200

    add BahnDSL GNU GPL3 copyright

--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/generator/yaml/BoardYamlExporter.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/generator/yaml/BoardYamlExporter.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of the BahnDSL project, a domain-specific language
+ * for configuring and modelling model railways
+ *
+ * BahnDSL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BahnDSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BahnDSL.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The following people contributed to the conception and realization of the
+ * present BahnDSL (in alphabetic order by surname):
+ *
+ * - Tri Nguyen <https://github.com/trinnguyen>
+ */
+
 package de.uniba.swt.dsl.common.generator.yaml;
 
 import de.uniba.swt.dsl.bahn.BoardElement;
