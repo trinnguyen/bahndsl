29cd3a2c03eda49c6de5f3cf30d04a6ffa0e2bcd
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-05-04 19:55:56 +0200

    fix spelling error

--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/layout/validators/LayoutElementValidator.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/layout/validators/LayoutElementValidator.java
@@ -1,4 +1,28 @@
 /*
+ *
+ *  Copyright (C) 2020 University of Bamberg, Software Technologies Research Group
+ *  <https://www.uni-bamberg.de/>, <http://www.swt-bamberg.de/>
+ *
+ * This file is part of the BahnDSL project, a domain-specific language
+ * for configuring and modelling model railways.
+ *
+ * BahnDSL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BahnDSL is a RESEARCH PROTOTYPE and distributed WITHOUT ANY WARRANTY, without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * The following people contributed to the conception and realization of the
+ * present BahnDSL (in alphabetic order by surname):
+ *
+ * - Tri Nguyen <https://github.com/trinnguyen>
+ *
+ */
+
+/*
  * This file is part of the BahnDSL project, a domain-specific language
  * for configuring and modelling model railways
  *
@@ -68,7 +92,7 @@ public class LayoutElementValidator {
             if (!reference.isNotSignal())
                 return;
 
-            throw new ValidationException(ValidationErrors.SinglaConnectorNoAllowed, BahnPackage.Literals.LAYOUT_REFERENCE__PROP);
+            throw new ValidationException(ValidationErrors.SignalConnectorNoAllowed, BahnPackage.Literals.LAYOUT_REFERENCE__PROP);
         }
 
         String prop = reference.getProp().getLiteral();
--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/validation/ValidationErrors.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/validation/ValidationErrors.java
@@ -1,4 +1,28 @@
 /*
+ *
+ *  Copyright (C) 2020 University of Bamberg, Software Technologies Research Group
+ *  <https://www.uni-bamberg.de/>, <http://www.swt-bamberg.de/>
+ *
+ * This file is part of the BahnDSL project, a domain-specific language
+ * for configuring and modelling model railways.
+ *
+ * BahnDSL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BahnDSL is a RESEARCH PROTOTYPE and distributed WITHOUT ANY WARRANTY, without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * The following people contributed to the conception and realization of the
+ * present BahnDSL (in alphabetic order by surname):
+ *
+ * - Tri Nguyen <https://github.com/trinnguyen>
+ *
+ */
+
+/*
  * This file is part of the BahnDSL project, a domain-specific language
  * for configuring and modelling model railways
  *
@@ -55,7 +79,7 @@ public class ValidationErrors {
 
     public final static String InvalidConnectorsFormat = "Invalid layout connector, use: '%s'";
 
-    public final static String SinglaConnectorNoAllowed = "Connector is not supported in signal";
+    public final static String SignalConnectorNoAllowed = "Connector is not supported in signal";
 
     public final static String NotSupportedConnectorElementFormat = "Connector is allowed for signal, point, block, platform, crossing, unexpected '%s'";
 
