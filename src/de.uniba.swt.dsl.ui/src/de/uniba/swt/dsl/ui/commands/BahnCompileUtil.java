7d633fcf2b1715ccba946d04dab1b967cdf6762a
Tri Nguyen <tri@trinnguyen.com>
2021-04-18 15:38:24 +0200

    Update progress dialog for Eclipse job

--- src/de.uniba.swt.dsl.ui/src/de/uniba/swt/dsl/ui/commands/BahnCompileUtil.java
+++ src/de.uniba.swt.dsl.ui/src/de/uniba/swt/dsl/ui/commands/BahnCompileUtil.java
@@ -6,16 +6,17 @@ import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.progress.IProgressService;
-import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
 import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
 
 import com.google.inject.Injector;
 
@@ -32,36 +33,52 @@ public class BahnCompileUtil {
 		
 		// start generating
     	if (!files.isEmpty()) {
-    		IWorkbench wb = PlatformUI.getWorkbench();
-			IProgressService ps = wb.getProgressService();
-			try {
-				ps.busyCursorWhile(new IRunnableWithProgress() {
-					public void run(IProgressMonitor pm) {
-						pm.beginTask("Generate C code using SCCharts", files.size());
-						
-						// init
-						Injector injector = BahnStandaloneSetup.doSetup();
-						StandaloneApp app = injector.getInstance(StandaloneApp.class);
-						JavaIoFileSystemAccess fsa = injector.getInstance(JavaIoFileSystemAccess.class);
-						
-						// process
-						for (IFile file : files) {
-							var path = file.getLocation().toOSString();
-							pm.subTask("Process file " + path);
-							app.runGenerator(path, fsa, null, mode);
-							pm.worked(1);
-						}
+    		var wb = PlatformUI.getWorkbench();
+    		var service = wb.getProgressService();
+    		try {
+				service.busyCursorWhile(new IRunnableWithProgress() {
+					
+					@Override
+					public void run(IProgressMonitor monitor) {
+						process(files, mode, monitor);
 					}
 				});
 			} catch (InvocationTargetException | InterruptedException e) {
-				e.printStackTrace();
-				StatusManager.getManager().handle(new Status(Status.WARNING, "de.uniba.swt.dsl.ui", e.getMessage(), e), StatusManager.LOG);
+				StatusManager.getManager().handle(new Status(Status.WARNING, "de.uniba.swt.dsl.ui", e.getMessage(), e), StatusManager.LOG);	
 			}
     	} else {
     		StatusManager.getManager().handle(new Status(Status.WARNING, "de.uniba.swt.dsl.ui", "No Bahn files is selected", null), StatusManager.LOG);
     	}
 	}
 	
+	private static void process(List<IFile> files, String mode, IProgressMonitor pm) {
+		pm.beginTask("Run Bahn compiler with mode: " + mode, files.size());
+		
+		// init
+		Injector injector = BahnStandaloneSetup.doSetup();
+		StandaloneApp app = injector.getInstance(StandaloneApp.class);
+		JavaIoFileSystemAccess fsa = injector.getInstance(JavaIoFileSystemAccess.class);
+		
+		// process
+		for (IFile file : files) {
+			var path = file.getLocation().toOSString();
+			pm.subTask("Process file " + path);
+			app.runGenerator(path, fsa, null, mode);
+			
+			// refresh
+			try {
+				file.getProject().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+			} catch (CoreException e) {
+				e.printStackTrace();
+				StatusManager.getManager().handle(new Status(Status.WARNING, "de.uniba.swt.dsl.ui", e.getMessage(), e), StatusManager.LOG);
+			}
+			
+			// finish work
+			pm.worked(1);
+		}
+		pm.done();
+	}
+
 	private static List<IFile> getFiles(ExecutionEvent event) {
 		
 		var selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
