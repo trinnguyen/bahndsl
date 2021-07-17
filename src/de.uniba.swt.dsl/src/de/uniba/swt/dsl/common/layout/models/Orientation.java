8c8eb2940d8d398eb479b724726445f47658957e
Eugene Yip <eugene.yip@uni-bamberg.de>
2021-07-18 01:44:17 +0200

    Added orientation example

--- src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/layout/models/Orientation.java
+++ src/de.uniba.swt.dsl/src/de/uniba/swt/dsl/common/layout/models/Orientation.java
@@ -1,6 +1,11 @@
 package de.uniba.swt.dsl.common.layout.models;
 
+/* Orientation of a block, assuming the following up/down assignment:
+ * ---------------| |-----------------------| |-----------------
+ *  Block X    up | | down    Block Y    up | | down    Block Z
+ * ---------------| |-----------------------| |-----------------
+ */
 public enum Orientation {
-    AntiClockwise,  // right (down -> up)
-    Clockwise       // left (up -> down)
+    AntiClockwise,  // right (blockY.down -> blockY.up)
+    Clockwise       // left (blockY.up -> blockY.down)
 }
