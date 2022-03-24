f20a4ec104ec873444ecba281c0347a4c1680881
Tri Nguyen <tringuyen.nnt@gmail.com>
2022-03-24 23:28:57 +0700

    Resolves #37: Add macosx aarch64 build for Bahn IDE

--- src/build-maven-rcp.sh
+++ src/build-maven-rcp.sh
@@ -10,6 +10,7 @@ cd build || exit 1
 
 # move
 echo "Move all artifacts to build folder"
+cp ../de.uniba.swt.dsl.product.rcp/target/products/BahnIDE-macosx.cocoa.aarch64.tar.gz ./BahnIDE-macosx.cocoa.aarch64.tar.gz
 cp ../de.uniba.swt.dsl.product.rcp/target/products/BahnIDE-macosx.cocoa.x86_64.tar.gz ./BahnIDE-macosx.cocoa.x86_64.tar.gz
 cp ../de.uniba.swt.dsl.product.rcp/target/products/BahnIDE-linux.gtk.x86_64.tar.gz ./BahnIDE-linux.gtk.x86_64.tar.gz
 cp ../de.uniba.swt.dsl.product.rcp/target/products/BahnIDE-win32.win32.x86_64.zip ./BahnIDE-win32.win32.x86_64.zip
