6cfbed98a002a9cac8e6aee7ffb732ffcc1c6bb7
Eugene Yip <eyip002@aucklanduni.ac.nz>
2022-10-05 21:49:50 +0200

    Updated to Java 17 (Xtext still limited to Java 11). Updated Xtext to 2.28

--- src/build-gradle.sh
+++ src/build-gradle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 
 cd "$(dirname "$0")" || exit 1
 ./gradlew de.uniba.swt.dsl:clean de.uniba.swt.dsl:build || exit 1
